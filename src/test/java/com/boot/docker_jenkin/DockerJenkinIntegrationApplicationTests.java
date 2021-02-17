2d4cafee295ee139e305744c75c10bef187583f2
aswartha <palla.aswartha@gmail.com>
2021-02-17 12:30:08 +0530

    Third commit

--- src/test/java/com/boot/docker_jenkin/DockerJenkinIntegrationApplicationTests.java
+++ src/test/java/com/boot/docker_jenkin/DockerJenkinIntegrationApplicationTests.java
@@ -14,6 +14,7 @@ class DockerJenkinIntegrationApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.error("Test case Executing........");
+		logger.error("Test case Executing........Second Time");
 		assertEquals(true, true);
 	}
 

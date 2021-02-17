e212cfe0a3b7e1abe235506d724506db97e000ce
aswartha <palla.aswartha@gmail.com>
2021-02-17 11:57:26 +0530

    second commit

--- src/test/java/com/boot/docker_jenkin/DockerJenkinIntegrationApplicationTests.java
+++ src/test/java/com/boot/docker_jenkin/DockerJenkinIntegrationApplicationTests.java
@@ -1,13 +1,20 @@
 package com.boot.docker_jenkin;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DockerJenkinIntegrationApplicationTests {
+	public static final Logger logger = LoggerFactory.getLogger(DockerJenkinIntegrationApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.error("Test case Executing...");
+		assertEquals(true, true);
 	}
 
 }
